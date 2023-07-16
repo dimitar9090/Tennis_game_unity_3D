4431f0e1e3f0c2877dd17207435971ab4f3d183d
Dimitar Anguelov <d.angelov9090@gmail.com>
2023-07-17 01:48:33 +0300

    making better bot

--- Tennis/Assets/scripts/bot.cs
+++ Tennis/Assets/scripts/bot.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class bot : MonoBehaviour
 {
-    float speed = 50;
+    float speed = 40;
     Animator animator;
     public Transform ball;
     public Transform aimTarget;
+
+    public Transform[] targets;
+
     float force = 13;
     Vector3 targetPosition;
+
+    ShotManager shotManager;
     // Start is called before the first frame update
     void Start()
     {
         targetPosition = transform.position;
         animator = GetComponent<Animator>();
+        shotManager = GetComponent<ShotManager>();
     }
 
     // Update is called once per frame
@@ -29,12 +35,28 @@ public class bot : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
     }
 
+    Vector3 PickTarget()
+    {
+        int randomvalue = Random.Range(0, targets.Length);
+        return targets[randomvalue].position;
+    }
+
+    Shot PickShot()
+    {
+        int randomvalue = Random.Range(0, 2);
+        if (randomvalue == 0)
+            return shotManager.topSpin;
+        else 
+            return shotManager.flat;
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Ball"))
         {
-            Vector3 dir = aimTarget.position - transform.position;
-            other.GetComponent<Rigidbody>().velocity = dir.normalized * force + new Vector3(0, 5, 0);
+            Shot currentshot = PickShot();
+
+            Vector3 dir = PickTarget() - transform.position;
+            other.GetComponent<Rigidbody>().velocity = dir.normalized * currentshot.hitForce + new Vector3(0, currentshot.upForce, 0);
 
             Vector3 ballDir = ball.position - transform.position;
 
